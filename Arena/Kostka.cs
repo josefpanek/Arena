e8fa2ca21ad0e2b518bf7de67af791c62e1d1859
Pepa Pánek <josef.panek@gmail.com>
2022-05-06 18:53:41 +0200

    Add project files.

---
+++ Arena/Kostka.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arena
+{
+    /// <summary>
+    /// Třída reprezentuje hrací kostku
+    /// </summary>
+    class Kostka
+    {
+        /// <summary>
+        /// Generátor náhodných čísel
+        /// </summary>
+        private Random random;
+        /// <summary>
+        /// Počet stěn kostky
+        /// </summary>
+        private int pocetSten;
+        public override string ToString()
+        {
+            return String.Format("Kostka s {0} stěnami", pocetSten);
+        }
+        /// <summary>
+        /// Vykoná hod kostkou
+        /// </summary>
+        /// <returns>Číslo od 1 do počtu stěn</returns>
+        public Kostka()
+        {
+            pocetSten = 6;
+            random = new Random();
+        }
+        /// <summary>
+        /// Vykoná hod kostkou
+        /// </summary>
+        /// <returns>Číslo od 1 do počtu stěn</returns>
+        public Kostka(int pocetSten)
+        {
+            this.pocetSten = pocetSten;
+            random = new Random();
+        }
+        /// <summary>
+        /// Vrátí počet stěn hrací kostky
+        /// </summary>
+        /// <returns>počet stěn hrací kostky</returns>
+        public int VratPocetSten()
+        {
+            return pocetSten;
+        }
+        /// <summary>
+        /// Vykoná hod kostkou
+        /// </summary>
+        /// <returns>Číslo od 1 do počtu stěn</returns>
+        public int Hod()
+        {
+            return random.Next(1, pocetSten + 1);
+        }
+    }
+}
---
+++ Arena/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arena
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kostka sestistenna = new Kostka();
+            Kostka desetistenna = new Kostka(10);
+
+            Console.WriteLine(sestistenna);
+            for (int i = 0; i < 50; i++)
+                Console.Write(sestistenna.Hod() + " ");
+            Console.WriteLine();
+            Console.WriteLine(desetistenna);
+            for (int i = 0; i < 50; i++)
+                Console.Write(desetistenna.Hod() + " ");
+            Console.ReadKey();
+        }
+    }
+}
